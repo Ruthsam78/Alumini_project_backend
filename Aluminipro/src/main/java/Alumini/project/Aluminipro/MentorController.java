a950a0d10724834f03ae25b5dd175b25285e7004
Ruthran U <ruthranu072@gmail.com>
2026-06-24 20:21:18 +0530

    Update MentorController.java

--- Aluminipro/src/main/java/Alumini/project/Aluminipro/MentorController.java
+++ Aluminipro/src/main/java/Alumini/project/Aluminipro/MentorController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/mentor")
-@CrossOrigin(origins = "*")
 public class MentorController {
 
     @Autowired
